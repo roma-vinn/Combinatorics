b754972b2db7a4ca91401bb51f2b04fa8281ff70
Roman Polishchenko <romanpolishchenko@knu.ua>
2018-11-16 21:39:20 +0200

    test commit

---
+++ test.py
@@ -0,0 +1,9 @@
+"""
+by Roman Polishchenko
+2 course, comp math
+Taras Shevchenko National University of Kyiv
+email: roma.vinn@gmail.com
+"""
+
+if __name__ == '__main__':
+    print('Hello world!')
