65a5df6d4f4b63eeb8451b9d50008497cb4be2f3
Roman Polishchenko <roma.vinn@gmail.com>
2018-11-16 23:29:38 +0200

    added rod cutting task with the cost of the cut

---
+++ Cut Rod/cut_rod_bottom_up.py
@@ -0,0 +1,51 @@
+"""
+by Roman Polishchenko
+2 course, comp math
+Taras Shevchenko National University of Kyiv
+email: roma.vinn@gmail.com
+"""
+# A Dynamic Programming solution for Rod cutting problem
+INT_MIN = float('-inf')
+
+
+def cut_rod(price, n, cut_cost=0):
+    """
+    Returns the best obtainable price for a rod of length n and
+    price[] as prices of different pieces if the cost of any cut is
+    cut_cost
+    :param price: array of prices
+    :param n: length of rod
+    :param cut_cost: cost of any cut (default = 0)
+    :return: (best price, required pieces)
+    """
+    val = [0] * (n + 1)
+    _cuts = [[0]] * (n + 1)
+
+    # Build the table val[] in bottom up manner and return
+    # the last entry from the table
+    for i in range(1, n + 1):
+        max_val = INT_MIN
+        for j in range(i):
+            if max_val <= price[j] + val[i - j - 1] - cut_cost:
+                _cuts[i] = _cuts[i - j - 1] + [j + 1]
+                max_val = price[j] + val[i - j - 1] - cut_cost
+        val[i] = max_val
+
+    # if 0 cutting price is better, then return it
+    if price[n-1] > val[n]:
+        return price[n-1], [n]
+
+    return val[n], _cuts[-1][1:]
+
+
+def test():
+    # test
+    arr = [1, 5, 8, 9, 10, 17, 17, 20]
+    size = len(arr)
+    revenue, cuts = cut_rod(arr, size)
+    print("Maximum Obtainable Value is", revenue)
+    print('Optimal split is', cuts)
+
+
+if __name__ == '__main__':
+    test()
